dcae07edafffe802181792043539a55512d64eb6
Ytallo Layon <ytallo.layon@gmail.com>
2025-11-12 11:04:36 -0300

    feat(adapter-bullmq-events): update BullMQ and ioredis dependencies, refactor event adapter to use new connection, queue, and worker managers

---
+++ packages/adapter-bullmq-events/src/constants.ts
@@ -0,0 +1,10 @@
+export const DEFAULT_CONCURRENCY = 5
+export const DEFAULT_ATTEMPTS = 3
+export const DEFAULT_BACKOFF_DELAY = 2000
+export const DEFAULT_REMOVE_ON_COMPLETE_COUNT = 1000
+export const DEFAULT_REMOVE_ON_FAIL_COUNT = 5000
+export const DEFAULT_PREFIX = 'motia'
+export const FIFO_CONCURRENCY = 1
+export const MILLISECONDS_PER_SECOND = 1000
+
+export const LOG_PREFIX = '[BullMQ]'
