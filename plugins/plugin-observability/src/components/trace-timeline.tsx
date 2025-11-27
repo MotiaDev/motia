672c6c19cf5b08b3537ccc44d28115082d884782
Glauber Castro <gfc714@gmail.com>
2025-11-27 17:31:23 -0300

    feat: improve zoom functionality in the tracing tab (#983)

--- plugins/plugin-observability/src/components/trace-timeline.tsx
+++ plugins/plugin-observability/src/components/trace-timeline.tsx
@@ -3,7 +3,7 @@ import { Button } from '@motiadev/ui'
 import Minus from 'lucide-react/icons/minus'
 import Plus from 'lucide-react/icons/plus'
 import type React from 'react'
-import { memo, useCallback, useMemo, useState } from 'react'
+import { memo, useMemo, useState } from 'react'
 import { useGetEndTime } from '../hooks/use-get-endtime'
 import { formatDuration } from '../lib/utils'
 import { useObservabilityStore } from '../stores/use-observability-store'
@@ -32,53 +32,41 @@ const TraceTimelineComponent: React.FC<TraceTimelineComponentProps> = memo(({ gr
   const { data } = useStreamGroup<Trace>(streamGroupArgs)
 
   const endTime = useGetEndTime(group)
-  const [zoom, setZoom] = useState(1)
+  const [zoom, setZoom] = useState(100)
   const selectedTraceId = useObservabilityStore((state) => state.selectedTraceId)
   const selectTraceId = useObservabilityStore((state) => state.selectTraceId)
 
   const selectedTrace = useMemo(() => data?.find((trace) => trace.id === selectedTraceId), [data, selectedTraceId])
 
-  const zoomMinus = useCallback(() => {
-    if (zoom > 0.5) setZoom((prev) => prev - 0.1)
-  }, [])
-
-  const zoomPlus = useCallback(() => {
-    setZoom((prev) => prev + 0.1)
-  }, [])
+  const zoomMinus = () => zoom > 100 && setZoom((prevZoom) => prevZoom - 10)
+  const zoomPlus = () => zoom < 200 && setZoom((prevZoom) => prevZoom + 10)
 
   if (!group) return null
 
   return (
     <>
-      <div className="flex flex-col flex-1 overflow-x-auto h-full relative">
-        <div className="flex flex-col items-center min-w-full sticky top-0" style={{ width: `${zoom * 1000}px` }}>
-          <div className="flex flex-1 w-full sticky top-0 bg-background z-10">
-            <div className="w-full min-h-[37px] h-[37px] min-w-[200px] max-w-[200px] flex items-center justify-center gap-2 sticky left-0 top-0 bg-card backdrop-blur-[4px] backdrop-filter">
-              <Button variant="icon" size="sm" className="px-2" onClick={zoomMinus}>
+      <div className="flex flex-col flex-1 relative min-h-full min-w-[1000px]">
+        <div className="flex flex-col" style={{ width: `${zoom}%` }}>
+          <div className="flex flex-1 bg-background" style={{ width: `${zoom}%` }}>
+            <div className="shrink-0 w-[200px] h-[37px] flex items-center justify-center gap-2 sticky left-0 top-0 z-10 bg-card backdrop-blur-xs backdrop-filter">
+              <Button variant="icon" size="sm" className="px-2" onClick={zoomMinus} disabled={zoom <= 100}>
                 <Minus className="w-4 h-4 cursor-pointer" />
               </Button>
-              <span className="text-sm font-bold text-muted-foreground">{Math.floor(zoom * 100)}%</span>
-              <Button variant="icon" size="sm" className="px-2" onClick={zoomPlus}>
+              <span className="min-w-12 text-center select-none text-sm font-bold text-muted-foreground">{zoom}%</span>
+              <Button variant="icon" size="sm" className="px-2" onClick={zoomPlus} disabled={zoom >= 200}>
                 <Plus className="w-4 h-4 cursor-pointer" />
               </Button>
             </div>
             <div className="flex justify-between font-mono p-2 w-full text-xs text-muted-foreground bg-card">
-              <span>{formatDuration(0)}</span>
+              <span>0ms</span>
               <span>{formatDuration(Math.floor((endTime - group.startTime) * 0.25))}</span>
               <span>{formatDuration(Math.floor((endTime - group.startTime) * 0.5))}</span>
               <span>{formatDuration(Math.floor((endTime - group.startTime) * 0.75))}</span>
               <span>{formatDuration(Math.floor(endTime - group.startTime))}</span>
-              <div className="absolute bottom-[-4px] w-full flex justify-between">
-                <span className="w-[1px] h-full bg-blue-500"></span>
-                <span className="w-[1px] h-full bg-blue-500"></span>
-                <span className="w-[1px] h-full bg-blue-500"></span>
-                <span className="w-[1px] h-full bg-blue-500"></span>
-                <span className="w-[1px] h-full bg-blue-500"></span>
-              </div>
             </div>
           </div>
 
-          <div className="flex flex-col w-full h-full">
+          <div className="flex flex-col h-full" style={{ width: `${zoom}%` }}>
             {data?.map((trace) => (
               <TraceItem
                 key={trace.id}
