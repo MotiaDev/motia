b2d6889a8f3e20989791459602f1f03e134b5aa9
Ytallo Layon <ytallo.layon@gmail.com>
2025-12-01 16:31:48 -0300

    chore(global-setup): update stdio options for execSync commands to inherit from parent process

--- packages/e2e/scripts/pr-global-setup.ts
+++ packages/e2e/scripts/pr-global-setup.ts
@@ -27,6 +27,7 @@ async function globalSetup() {
     const createCommand = `node ${cliPath} create  ${TEST_PROJECT_NAME} -t ${template}`
 
     execSync(createCommand, {
+      stdio: 'inherit',
       cwd: path.join(ROOT_PATH, 'packages'),
     })
 
@@ -62,6 +63,7 @@ async function globalSetup() {
     // execSync('pnpm build', { cwd: ROOT_PATH, stdio: 'pipe' })
     execSync(`pnpm install --filter ${TEST_PROJECT_NAME}`, {
       cwd: ROOT_PATH,
+      stdio: 'inherit',
       env: {
         ...process.env,
         CI: 'false',
