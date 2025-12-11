819106388c46d379e5d49671035b2cffd0b2f665
Ytallo Layon <ytallo.layon@gmail.com>
2025-12-11 01:41:39 -0300

    fix: update getPackageManager to support optional directory traversal (#1046)

--- packages/snap/src/utils/get-package-manager.ts
+++ packages/snap/src/utils/get-package-manager.ts
@@ -37,7 +37,7 @@ const readPackageManagerFromPackageJson = (dir: string): string | null => {
   return null
 }
 
-export const getPackageManager = (dir: string): string => {
+export const getPackageManager = (dir: string, traverseUp = true): string => {
   const envPackageManager = getPackageManagerFromEnv()
   if (envPackageManager) {
     return envPackageManager
@@ -59,6 +59,10 @@ export const getPackageManager = (dir: string): string => {
     const packageManagerFromJson = readPackageManagerFromPackageJson(currentDir)
     if (packageManagerFromJson) {
       return packageManagerFromJson
+    }
+
+    if (!traverseUp) {
+      break
     }
 
     currentDir = path.dirname(currentDir)
