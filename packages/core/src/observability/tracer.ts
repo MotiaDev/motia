1be7ecd35adb456a860867279fabfd266da942ef
Ytallo <ytallo123@hotmail.com>
2025-12-11 21:05:36 -0300

    feat: trace refactor (#1058)

--- packages/core/src/observability/tracer.ts
+++ packages/core/src/observability/tracer.ts
@@ -40,19 +40,13 @@ export class BaseTracerFactory implements TracerFactory {
     const traceGroup: TraceGroup = {
       id: traceId,
       name: step.config.name,
-      lastActivity: Date.now(),
-      metadata: {
-        completedSteps: 0,
-        activeSteps: 0,
-        totalSteps: 0,
-      },
-      correlationId: undefined,
-      status: 'running',
       startTime: Date.now(),
     }
 
     const trace = createTrace(traceGroup, step)
     const manager = new TraceManager(this.traceStream, this.traceGroupStream, traceGroup, trace)
+
+    await manager.updateTraceGroup()
 
     return new StreamTracer(manager, traceGroup, trace, logger)
   }
