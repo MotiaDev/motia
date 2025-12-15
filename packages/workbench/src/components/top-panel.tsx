654d2db41a56e75a88635f96b1329ea89bc94393
Anderson Leal <andersonofl@gmail.com>
2025-12-15 15:09:56 -0300

    feat: add TabErrorBoundary component for error handling in tab content

--- packages/workbench/src/components/top-panel.tsx
+++ packages/workbench/src/components/top-panel.tsx
@@ -3,6 +3,7 @@ import { memo } from 'react'
 import { useShallow } from 'zustand/react/shallow'
 import { type AppTabsState, TabLocation, useAppTabsStore } from '../stores/use-app-tabs-store'
 import { useTabsStore } from '../stores/use-tabs-store'
+import { TabErrorBoundary } from './tab-error-boundary'
 
 const topTabsSelector = (state: AppTabsState) => state.tabs[TabLocation.TOP]
 const topPanelId = 'top-panel'
@@ -31,7 +32,9 @@ export const TopPanel = memo(() => {
     >
       {tabs.map(({ id, content: Element }) => (
         <TabsContent key={id} value={id} className="h-full">
-          <Element />
+          <TabErrorBoundary tabId={id}>
+            <Element />
+          </TabErrorBoundary>
         </TabsContent>
       ))}
     </CollapsiblePanel>
