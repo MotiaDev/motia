f1b0706492d3dbe4bd12d84a7e6c7e758d443d52
anthonyiscoding <142696453+anthonyiscoding@users.noreply.github.com>
2026-01-13 15:59:19 -0600

    cleanup footer

--- website/pages/SectionsPreview.tsx
+++ website/pages/SectionsPreview.tsx
@@ -31,7 +31,6 @@ export function SectionsPreview() {
         <WhatsCatchSection />
         <FAQSection />
         <CommunitySection />
-        <CTAFooterSection />
       </div>
     </div>
   );
